021cbc0d1d35e5c862d6c41715a257769fcf20e5
RohanChowke <chowkerohan@gmail.com>
2023-06-24 21:20:00 +0530

    add all code

---
+++ background.js
@@ -0,0 +1,6 @@
+// background.js
+// Author: Chowke Rohan
+// Author Github URI: https://github.com/RohanChowke
+// Project Repository URI: https://github.com/RohanChowke/AZ-Chrome-extension
+// Description: Handles all the browser level activities (e.g. tab management, etc.)
+// License: MIT
